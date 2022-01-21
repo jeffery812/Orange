6b08f3fb145179d9d24778c9ae139a750599e6d2
Zhihui Tang <crafttang@gmail.com>
2022-01-21 20:21:56 +0100

    Add menu

--- Orange/AppDelegate.swift
+++ Orange/AppDelegate.swift
@@ -9,12 +9,10 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -24,7 +22,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         return true
     }
+    
+    private func constructMenu() {
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("statusItem_icon"))
+        }
 
-
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Help", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "H"))
+        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(_:)), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+    
+    @objc func printQuote(_ sender: Any?) {
+      let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+      let quoteAuthor = "Mark Twain"
+      
+      print("\(quoteText) — \(quoteAuthor)")
+    }
 }
 
