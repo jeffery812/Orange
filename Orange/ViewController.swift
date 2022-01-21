a115c04ef21090ed4414ea55831ec37e02651e38
Zhihui Tang <crafttang@gmail.com>
2022-01-21 20:15:29 +0100

    Init

---
+++ Orange/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Orange
+//
+//  Created by Zhihui Tang on 2022-01-21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
